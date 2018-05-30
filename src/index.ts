8178307430974d40db25103ac509871bf8b4f8f1
Robey Pointer <robeypointer@gmail.com>
2018-05-29 18:23:43 -0700

    oops, export this type.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // in case the ES8 engine doesn't have the AsyncIterator symbol yet.
 (Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
 
+export { AlertingAsyncIterator } from "./alerting_async_iterator";
 export { asyncIter, ExtendedAsyncIterable } from "./extended_async_iterable";
 export { PushAsyncIterator } from "./push_async_iterator";
 export { StreamAsyncIterator } from "./stream_async_iterator";
